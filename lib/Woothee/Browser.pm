de3633b605ab1414a4025242b65790383fbc334e
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-04-06 14:57:54 +0900

    Checking in changes prior to tagging of version 0.4.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b1d9430..b939c56 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl module Woothee
    
     {{$NEXT}}
    
    +0.4.0 2014-04-06T05:57:48Z
    +   - bump version up (w/ other lang imps)
    +
     0.3.9 2014-04-02T03:51:04Z
        - fix Opera (w/ blink) support

--- lib/Woothee/Browser.pm
+++ lib/Woothee/Browser.pm
@@ -7,7 +7,7 @@ use Carp;
 use Woothee::Util qw/update_map update_category update_version update_os/;
 use Woothee::DataSet qw/dataset/;
 
-our $VERSION = "0.3.9";
+our $VERSION = "0.4.0";
 
 sub challenge_msie {
     my ($ua,$result) = @_;
